638c948c939ee86d73eab35269da15194b299736
Pete Hunt <floydophone@gmail.com>
2023-01-28 01:55:24 +0000

    Migrate to Definitions

--- recommender/recommender/__init__.py
+++ recommender/recommender/__init__.py
@@ -1,1 +1,5 @@
-from .repository import recommender
+from dagster import Definitions, load_assets_from_modules
+
+from . import assets
+
+defs = Definitions(assets=load_assets_from_modules([assets]))
--- recommender/recommender/repository.py
+++
@@ -1,8 +0,0 @@
-from dagster import load_assets_from_package_module, repository
-
-from recommender import assets
-
-
-@repository
-def recommender():
-    return [load_assets_from_package_module(assets)]
